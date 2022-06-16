1d70eb81191fbafe89f43da929c8e1fd0590e394
Judy Nkwama <nkwamajudy@gmail.com>
2022-06-16 15:38:49 +0300

    adding a comment

--- part2/App.js
+++ part2/App.js
@@ -7,3 +7,5 @@ App.use(express.static("public"));
 
 const PORT = process.env.PORT || 5000;
 App.listen(PORT, ()=>{console.log(`Listeing on ${PORT}...`)});
+
+//This is just a comment line to test gitHub features
