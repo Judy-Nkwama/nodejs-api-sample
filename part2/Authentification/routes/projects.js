b3e468eb04e100d29d4e9a14479463ea48e5cd26
Judy Nkwama <nkwamajudy@gmail.com>
2022-06-29 10:18:23 +0300

    this morning

---
+++ part2/Authentification/routes/projects.js
@@ -0,0 +1,31 @@
+
+
+
+const myTenantID = "MeganB@M365B320322.OnMicrosoft.com";
+
+
+
+const express = require('express')
+const router = express.Router()
+const { projects } = require('../data')
+
+router.get('/', (req, res) => {
+  res.json(projects)
+})
+
+router.get('/:projectId', setProject, (req, res) => {
+  res.json(req.project)
+})
+
+function setProject(req, res, next) {
+  const projectId = parseInt(req.params.projectId)
+  req.project = projects.find(project => project.id === projectId)
+  
+  if (req.project == null) {
+    res.status(404)
+    return res.send('Project not found')
+  }
+  next()
+}
+
+module.exports = router
